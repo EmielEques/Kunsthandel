6fa027cb364c4440dcc89a22c63a09fd5e80f508
EmielEques <deruijter.emiel@gmail.com>
2026-06-19 15:26:19 +0200

    Create data.ts

---
+++ components/data.ts
@@ -0,0 +1,8 @@
+export { chapters, objects, siteSettings } from '@/content/site';
+
+export const navigation = [
+  { label: 'Home', href: '/' },
+  { label: 'Chapter 1', href: '/chapter-1' },
+  { label: 'Chapter 2', href: '/chapter-2' },
+  { label: 'Contact', href: '/contact' },
+];
